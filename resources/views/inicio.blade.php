6e9f4681b299613594ef1678ab64a4425dea0011
Andres Varela <andresvarela26@gmail.com>
2015-06-10 02:43:09 -0430

    commit inicial

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,317 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+<title>Naturarte</title>
+
+<!-- Bootstrap Core CSS -->
+<link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet">
+
+<!-- Custom CSS -->
+<link href="{{ asset('/css/five.css') }}" rel="stylesheet">
+
+
+<!-- Custom Fonts -->
+<link href="font-awesome-4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+<link href='http://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,400italic,700,600,600italic' rel='stylesheet' type='text/css'>
+
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top" class="index">
+
+<!-- Navigation -->
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container"> 
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header page-scroll">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-2"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+    </div>
+    <span class="navbar-header page-scroll"><a class="navbar-brand page-scroll" href="#page-top">
+      <div class="logo">
+        <img src="img/logo.jpg"  width='250' height='99'>
+      </div>
+    </a></span> 
+    
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li class="hidden"> <a href="#page-top"></a> </li>
+        <li> <a class="page-scroll" href="#services">Servicios</a> </li>
+         <li> <a class="page-scroll" href="#about">Quienes somos</a> </li>
+        <li> <a class="page-scroll" href="#portfolio">Sectores</a> </li>
+        <li> <a class="page-scroll" href="#contact">Contacto</a> </li>
+      </ul>
+    </div>
+    <!-- /.navbar-collapse --> 
+  </div>
+  <!-- /.container-fluid --> 
+</nav>
+
+<!-- Header -->
+<header>
+  <div class="container">
+    <div class="intro-text">
+      <div class="intro-heading">Líderes en Mantenimiento Preventivo</div>
+      <div class="intro-lead-in">Satisfaciendo las necesidades de preservacion y mantenimiento de infraestructura</div>
+      
+     <!-- <a href="#services" class="page-scroll btn btn-xl"><i class="fa fa-angle-double-down fa-4x"></i></a>--></div> 
+
+  </div>
+</header>
+<!-- Services Section -->
+<section id="services" class="bg-light-gray">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 text-center">
+        <h2 class="section-heading">Servicios</h2>
+         <div class="intro-lead-in">Servicios 100% personalizados a tu empresa</div>
+        <div class="line"></div>
+      </div>
+    </div>
+    <div class="col-md-4 col-sm-6 portfolio-item"> <a class="portfolio-link" data-toggle="modal">
+     <img src="img/poliza.jpg" class="img-rounded" alt=" " width='300' style="position:relative; left:30px"></a> 
+     <div class="portfolio-caption">
+          <h4 align="center">PÓLIZA DE MANTENIMIENTO PREVENTIVO</h4>
+          <p  align="center" class="text-muted">Una póliza de mantenimiento integral preventivo y correctivo a nivel coorporativo. 
+            Cubre todas las necesidades del cliente mediante métodos y procedimientos que aseguran los más altos estándares de calidad y servicio, garan#zando excelentes resultados, bajo un presupuesto fijo altamente competitivo.
+            Abarca todas las áreas de mantenimiento; civil, eléctrico, sanitario, electromecánico y de equipamiento.</p>
+        </div>  
+      </div>
+         <div class="col-md-4 col-sm-6 portfolio-item"> <a class="portfolio-link" data-toggle="modal">
+        <img src="img/facilities.jpg" class="img-rounded" alt=""  width='300' style="position:relative; left:30px"> </a>
+        <div class="portfolio-caption">
+          <h4 align="center">Facility Management</h4>
+          <p  align="center" class="text-muted">Somos pioneros en servicios de “Outsourcing” en “Facility Management”. 
+            Nuestro objetivo es adaptarnos a sus necesidades buscando garantizar un excelente ambiente de trabajo en sus oficinas a través de un novedoso sistema de mantenimiento integral. 
+            Actualmente hemos lanzado al mercado un producto único, con grandes posibilidades de desarrollo y funcionalidad internacional adaptado a cada cliente, haciendo de éste un servicio versátil y  completo con la solidez que nos caracteriza como empresa de amplia trayectoria en el ramo.</p>
+        </div>    
+      </div>
+      
+       <div class="col-md-4 col-sm-6 portfolio-item"> <a class="portfolio-link" data-toggle="modal">
+        <img src="img/ingenieria.jpg" class="img-rounded" alt=""  width='300' style="position:relative; left:30px"> </a>  
+        <div class="portfolio-caption">
+          <h4 align="center">imagen e ingeniería ambiental</h4>
+          <p  align="center" class="text-muted">Para adaptar las exigencias del ambiente con la imperiosa necesidad de desarrollo del mismo, nuestra empresa, constituida como unidad estratégica de negocios, ofrece a cada cliente una gama de servicios. 
+            Entre éstos destacan desde la recreación de ambientes naturales internos hasta la elaboración de un complejo proyecto de recuperación forestal, reflejando así la excelencia profesional de nuestro compromiso con el medio ambiente.</p>
+        </div>     
+      </div>
+  </div>
+</section>
+<!-- About Section -->
+<section id="about" class="bg-light-gray">
+  <div class="container" >
+         <img src="img/empresa.jpg" class="img-responsive" alt=""  width='400' style="position:relative; top:-51px; left:-105px"> 
+         <div class="empresa">
+        <div id="empresa2">
+          <h2> LA EMPRESA</h2>
+          Fundadada en Caracas en 1987, Grupo NATURARTE presta servicios a nuestras empresas clientes en
+          <br> desarrollo de obras civiles y mantenimiento preventivo y correctivo. La empresa presta un servicio <br>
+          novedoso de gran seriedad con un alto sentido de preservación de los recursos, 
+          promoviendo una<br> integración entre el hombre productivo y su entorno.
+          <br><br><br>
+          <img src="img/ch1.png" class="img-responsive" alt=""  width='' style="position:relative; top:5px; left:-10px">
+          <div style="position:relative; left:42px; top:-53px" >Texto 1 Ejemplo </div>
+          <img src="img/ch1.png" class="img-responsive" alt=""  width='' style="position:relative; top:5px; left:-10px"> 
+          <div style="position:relative; left:42px; top:-53px" >Texto 2 Ejemplo </div>
+          <img src="img/ch1.png" class="img-responsive" alt=""  width='' style="position:relative; top:5px; left:-10px"> 
+          <div style="position:relative; left:42px; top:-53px" >Texto 3 Ejemplo </div>
+          <button type="submit" style="position:relative; top:-10px" text-aling="center" class="btn btn-xl">MÁS INFORMACIÓN</button>
+
+        </div>
+        </div>
+    
+  </div>
+</section>
+<!-- Portfolio Grid Section -->
+<section id="portfolio"class="bg-light-gray">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 text-center">
+        <h2 class="section-heading">Sectores</h2>
+        <div class="intro-lead-in">Atendemos necesidades de todos los sectores que estén a nuestro alcance</div>
+        <div class="line"></div>
+      </div>
+    </div>
+    <div class="row">
+       <div class="col-md-4">
+        <h4 class="about-heading" style="position:relative; top:-55px; left:60px">Sectores especializados</h4>
+        <p class="text-muted" style="position:relative; top:-75px; left:60px">Nuestra empresa se encarga de trabajar cade uno de los sectores industriales
+          de forma especializada. De esta forma logramos personalizar nuestros servicios a cada uno de nuestro clientes</p>
+      </div>
+       <img src="img/sectores.jpg" class="img-responsive" alt=""  width='450' style="position:relative; top:-5px; left:417px"> 
+       <img src="img/sectores3.jpg" class="img-responsive" alt=""  width='138' style="position:relative; top:-257px; left:100px"> 
+    </div>
+
+  </div>
+</section>
+<!-- Team Section -->
+<section id="team" class="bg-light-marron">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 text-center">
+        <h2 class="section-heading"><font color="white">Valores</h2>
+        <div class="intro-lead-in">Nuestra personalidad como empresa se rige bajo nuestros principales valores</div></font>
+      </div>
+    </div>
+     <img src="img/valores.jpg" class="img-responsive" alt=""  width='750' style="position:relative; top:30px; left:170px">
+     <p style="position:relative; left:200px; top:25px"><font color="white"> SEGURIDAD</p></font>
+     <p style="position:relative; left:525px; top:-9px"><font color="white"> CALIDAD</p></font>
+     <p style="position:relative; left:830px; top:-42px"><font color="white"> EXCELENCIA</p></font>
+     <img src="img/valores1.jpg" class="img-responsive" alt=""  width='255' style="position:relative; top:-40px; left:110px">
+     <img src="img/valores2.jpg" class="img-responsive" alt=""  width='255' style="position:relative; top:-130px; left:745px">
+     <img src="img/valores3.jpg" class="img-responsive" alt=""  width='255' style="position:relative; top:-188px; left:430px">
+
+
+
+    </div>
+  </div>
+</section>
+
+<!-- CLIENTES-->
+<section id="clientes" class="bg-light-gray">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 text-center">
+<img src="img/clientes.jpg" class="img-responsive" alt=""  width='3500' style="position:relative; top:-100px;"> 
+</div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- Contacto Section -->
+<section id="contact">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 text-center">
+        <h2 class="section-heading"><font color="white">¡NOSOTROS TE CONTACTAMOS!</h2>
+      
+      </div>
+    </div>
+    
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-12">
+        <form name="sentMessage" id="contactForm" novalidate>
+          <div class="row">
+            <div class="col-md-6">
+              <div class="form-group">
+                <input type="email" class="form-control" style="position:relative; left:350px" placeholder="CORREO ELECTRÓNICO " id="email" required data-validation-required-message="COLOCAR CORREO ELECTRÓNICO.">
+                <p class="help-block text-danger"></p>
+              </div>
+            </div>
+              
+            </div>
+            <div class="clearfix"></div>
+            <div class="col-lg-12 text-center">
+              <div id="success"></div>
+              <button type="submit" class="btn btn-xl2">ENVIAR</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- Testimonio section-->
+<section id="testimonios" class="bg-light-gray">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 text-center">
+        <h2 class="section-heading">Testimonios</h2>
+        <div class="intro-lead-in">Comentarios de nuestros clientes satisfechos</div>
+        <div class="line"></div>
+               <img src="img/testimonio1.jpg" class="img-responsive" alt=""  width='450' style="position:relative; top:-5px; left:100px"> 
+               <img src="img/testimonio2.jpg" class="img-responsive" alt=""  width='450' style="position:relative; top:-125px; left:580px"> 
+               <img src="img/testimonio3.jpg" class="img-responsive" alt=""  width='450' style="position:relative; top:-100px; left:350px"> 
+    </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- pie de página-->
+<footer>
+  <div id="footer" class="bg-light-verde">
+  <div class="container">
+    <div class="row">
+     
+      <img src="img/footer.jpg" class="img-responsive" alt=""  width='800' style="position:relative; top:5px; left:130px"> 
+      <br>
+      <div class="col-md-4">
+        <i align=""  class="fa fa-map-marker fa-2x"></i>
+          Urb. La Lagunita, Av. Sur,<br>
+          <div style="position:relative; left:-7px" >Centro Empresarial<br></div>
+          <div style="position:relative; left:-30px" >La Lagunita,<br></div>
+          <div style="position:relative; left:-20px" >Piso 3, Ofic.312<br></div>
+         <div style="position:relative; left:-7px" > Caracas, Venezuela.</div>
+
+        <i  style="position:relative; right:85px; top:5px" class="fa fa-mobile fa-3x"></i>
+        <div style="position:relative; left:-7px; top:-23px">+58(0212)963 57 18</div>
+        <img src="img/icon.jpg" class="img-responsive" alt=""  width='25' style="position:relative; top:px; left:82px">
+        <div style="position:relative; top:-17px">info@naturarte.com</div>
+        <img src="img/sistema.jpg" class="img-responsive" alt=""  width='25' style="position:relative; top:-205px; left:475px">
+        <div style="position:relative; top:-225px; left:355px">Ingresa</div>
+        <img src="img/twitter.jpg" class="img-responsive" alt=""  width='25' style="position:relative; top:-245px; left:825px">
+
+
+
+    </div>
+  </div>
+   <div class="col-md-8"> 
+<font color="#555555" style="position:relative; top:80px; left:-125px;"><span class="copyright"><a href="" rel="nofollow"></a></span>© Copyright 2014 Naturarte.com </font>
+<font color="#555555" style="position:relative; top:80px; right:-430px;"><span class="copyright"><a href="" rel="nofollow"></a></span>Design y Developed by Awasero </font> 
+
+   <ul class="list-inline social-buttons">
+          <li style="position:relative; top:50px; left:180px"><a href="#"><i class="fa fa-twitter"></i></a> </li>
+          <li style="position:relative; top:50px; left:180px"><a href="#"><i class="fa fa-facebook"></i></a> </li>
+          <li style="position:relative; top:50px; left:180px"><a href="#"><i class="fa fa-google-plus"></i></a> </li>
+        </ul>
+   </div>
+</div>
+
+</footer>
+
+<!-- Portfolio Modals --> 
+<!-- Use the modals below to showcase details about your portfolio projects! --> 
+
+<!-- Portfolio Modal 1 -->
+
+
+<!-- jQuery --> 
+<script src="{{ asset('/js/jquery.js') }}"></script>
+
+<!-- Bootstrap Core JavaScript --> 
+<script src="{{ asset('/js/bootstrap.min.js') }}"></script>
+
+<!-- Plugin JavaScript --> 
+<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script> 
+<script src="{{ asset('/js/classie.js') }}"></script>
+
+<script src="{{ asset('/js/cbpAnimatedHeader.js') }}"></script> 
+
+<!-- Contact Form JavaScript --> 
+<script src="{{ asset('/js/jqbootstrapvalidation.js') }}"></script>
+<script src="{{ asset('/js/contact_me.js') }}"></script>
+
+
+<!-- Custom Theme JavaScript --> 
+
+<script src="{{ asset('/js/five.js') }}"></script>
+</body>
+</html>
